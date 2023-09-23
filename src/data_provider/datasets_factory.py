8e621f6b79a3199753237ea2f75a897d245de4ca
kyaFUK <flatisland.kuuchan@gmail.com>
2023-09-23 21:17:16 +0900

    modified

--- src/data_provider/datasets_factory.py
+++ src/data_provider/datasets_factory.py
@@ -26,7 +26,7 @@ def data_provider(dataset_name, train_data_paths, valid_data_paths, batch_size,
     Raises:
         ValueError: If `dataset_name` is unknown.
     '''
-    #pathを受け取り、データをラッパーラッパー(mnist.pyなど)に流す。
+    # taking path, and send data to wrapper (e.g. mnist.py)
     if dataset_name not in datasets_map:
         raise ValueError('Name of dataset unknown %s' % dataset_name)
     train_data_list = train_data_paths.split(',')
